6ee76233f40924b358b715911aa1c0e3c96a026c
Sergey Poznyakoff <gray@gnu.org>
2019-11-04 08:16:04 +0200

    Get rid of the unnecessary workaround.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,5 @@
 /* main.c - main program and argument processing for cpio.
-   Copyright (C) 1990-1992, 2001, 2003-2007, 2009-2010, 2014-2015, 2017
-   Free Software Foundation, Inc.
+   Copyright (C) 1990-2019 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -581,8 +580,6 @@ usage (int status)
 void
 process_args (int argc, char *argv[])
 {
-  void (*copy_in) ();		/* Work around for pcc bug.  */
-  void (*copy_out) ();
   int index;
 
   xstat = lstat;
@@ -601,11 +598,7 @@ process_args (int argc, char *argv[])
 		      _("You must specify one of -oipt options.")));
     }
 
-  /* Work around for pcc bug.  */
-  copy_in = process_copy_in;
-  copy_out = process_copy_out;
-
-  if (copy_function == copy_in)
+  if (copy_function == process_copy_in)
     {
       archive_des = 0;
       CHECK_USAGE (link_flag, "--link", "--extract");
@@ -637,7 +630,7 @@ process_args (int argc, char *argv[])
       if (input_archive_name)
 	archive_name = input_archive_name;
     }
-  else if (copy_function == copy_out)
+  else if (copy_function == process_copy_out)
     {
       if (index != argc)
 	USAGE_ERROR ((0, 0, _("Too many arguments")));
@@ -710,7 +703,7 @@ process_args (int argc, char *argv[])
 
   if (archive_name)
     {
-      if (copy_function != copy_in && copy_function != copy_out)
+      if (copy_function != process_copy_in && copy_function != process_copy_out)
 	error (PAXEXIT_FAILURE, 0, 
 	       _("-F can be used only with --create or --extract"));
       archive_des = open_archive (archive_name);
