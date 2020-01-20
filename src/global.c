641d3f489cf6238bb916368d4ba0d9325a235afb
Sergey Poznyakoff <gray@gnu.org>
2020-01-20 07:45:39 +0200

    Minor fix * src/global.c: Remove superfluous declaration of program_name

--- src/global.c
+++ src/global.c
@@ -184,9 +184,6 @@ unsigned int warn_option = 0;
 /* Extract to standard output? */
 bool to_stdout_option = false;
 
-/* The name this program was run with.  */
-char *program_name;
-
 /* A pointer to either lstat or stat, depending on whether
    dereferencing of symlinks is done for input files.  */
 int (*xstat) ();
