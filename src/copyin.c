19219d126ef04e23d4cc062032ce5a89ef1fde89
Paul Eggert <eggert@cs.ucla.edu>
2023-04-28 21:39:33 +0300

    Fix integer overflows in timestamp output
    * src/copyin.c (long_format): Fix some unlikely integer overflow bugs.
    Work even if (current_time - when) overflows, which is
    possible if time_t is unsigned.
    Do not assume that system integer types fit into unsigned long.
    Simplify by using %ju.  No need for local tbuf array.

--- src/copyin.c
+++ src/copyin.c
@@ -857,42 +857,46 @@ void
 long_format (struct cpio_file_stat *file_hdr, char const *link_name)
 {
   char mbuf[11];
-  char tbuf[40];
   time_t when;
+  char *tbuf;
+  enum { six_months = 6 * 30 * 24 * 60 * 60 };
 
   mode_string (file_hdr->c_mode, mbuf);
   mbuf[10] = '\0';
 
-  /* Get time values ready to print.  */
-  when = file_hdr->c_mtime;
-  strcpy (tbuf, ctime (&when));
-  if (current_time - when > 6L * 30L * 24L * 60L * 60L
-      || current_time - when < 0L)
-    {
-      /* The file is older than 6 months, or in the future.
-	 Show the year instead of the time of day.  */
-      strcpy (tbuf + 11, tbuf + 19);
-    }
-  tbuf[16] = '\0';
-
-  printf ("%s %3lu ", mbuf, (unsigned long) file_hdr->c_nlink);
+  printf ("%s %3ju ", mbuf, (uintmax_t) file_hdr->c_nlink);
 
   if (numeric_uid)
-    printf ("%-8u %-8u ", (unsigned int) file_hdr->c_uid,
-	    (unsigned int) file_hdr->c_gid);
+    printf ("%-8ju %-8ju ",
+	    (uintmax_t) file_hdr->c_uid,
+	    (uintmax_t) file_hdr->c_gid);
   else
     printf ("%-8.8s %-8.8s ", getuser (file_hdr->c_uid),
 	    getgroup (file_hdr->c_gid));
 
   if ((file_hdr->c_mode & CP_IFMT) == CP_IFCHR
       || (file_hdr->c_mode & CP_IFMT) == CP_IFBLK)
-    printf ("%3lu, %3lu ",
-	    (unsigned long) file_hdr->c_rdev_maj,
-	    (unsigned long) file_hdr->c_rdev_min);
+    printf ("%3ju, %3ju ",
+	    (uintmax_t) file_hdr->c_rdev_maj,
+	    (uintmax_t) file_hdr->c_rdev_min);
   else
-    printf ("%8"PRIuMAX" ", (uintmax_t) file_hdr->c_filesize);
+    printf ("%8ju ", (uintmax_t) file_hdr->c_filesize);
 
-  printf ("%s ", tbuf + 4);
+  /* Get time values ready to print.  Do not worry about ctime failing,
+     or a year outside the range 1000-9999, since 0 <= WHEN < 2**33.  */
+  when = file_hdr->c_mtime;
+  tbuf = ctime (&when);
+
+  if (!((current_time < six_months || current_time - six_months <= when)
+	&& when <= current_time))
+    {
+      /* The file is older than 6 months, or in the future.
+	 Show the year instead of the time of day.  */
+      memcpy (tbuf + 11, tbuf + 19, sizeof " 1970" - 1);
+    }
+  tbuf[16] = ' ';
+  tbuf[17] = '\0';
+  printf ("%s", tbuf + 4);
 
   printf ("%s", quotearg (file_hdr->c_name));
   if (link_name)
@@ -900,7 +904,7 @@ long_format (struct cpio_file_stat *file_hdr, char const *link_name)
       printf (" -> ");
       printf ("%s", quotearg (link_name));
     }
-  putc ('\n', stdout);
+  putchar ('\n');
 }
 
 /* Read a pattern file (for the -E option).  Put a list of
